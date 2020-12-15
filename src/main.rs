849cd8a960e6d5bd3a555bb8e0dc353fb7c39692
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-12-15 12:23:36 +1100

    Add progress bar

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use pt::rgb::Rgb;
 use rand::Rng;
 use rayon::prelude::*;
 use std::fs::File;
+use std::sync::mpsc;
+use std::thread;
 use ultraviolet::Vec3;
 
 const ASPECT_RATIO: f32 = 16.0 / 9.0;
@@ -18,6 +20,9 @@ const SAMPLES_PER_PIXEL: u16 = 100;
 const MAX_DEPTH: u16 = 50;
 
 fn main() -> anyhow::Result<()> {
+    let (tx, rx) = mpsc::channel();
+    let progress_bar_handle = thread::spawn(|| progress_bar(rx));
+
     let camera = {
         let look_from = Vec3::new(0.0, 0.0, 4.0);
         let look_at = Vec3::new(0.0, 0.0, -1.0);
@@ -93,7 +98,7 @@ fn main() -> anyhow::Result<()> {
         .flat_map(|y| (0..IMAGE_WIDTH).into_par_iter().map(move |x| (x, y)));
 
     let pixels: Vec<_> = image_coords
-        .map(|(x, y)| {
+        .map_with(tx, |tx, (x, y)| {
             let mut rng = rand::thread_rng();
 
             let mut pixel_color = Rgb::new(0.0, 0.0, 0.0);
@@ -107,6 +112,7 @@ fn main() -> anyhow::Result<()> {
                     ray_color(&world, &ray, Rgb::new(0.0, 0.0, 0.0), &mut rng, MAX_DEPTH);
             }
 
+            tx.send(()).unwrap();
             pixel_color
         })
         .collect();
@@ -118,7 +124,22 @@ fn main() -> anyhow::Result<()> {
 
     write_image("image.png", &u8s)?;
 
+    progress_bar_handle.join().unwrap();
+
     Ok(())
+}
+
+fn progress_bar(rx: mpsc::Receiver<()>) {
+    let total_pixels = usize::from(IMAGE_HEIGHT) * usize::from(IMAGE_WIDTH);
+
+    for (completed_pixels, _) in rx.into_iter().enumerate() {
+        print!(
+            "\r{:.4}%",
+            (completed_pixels as f32 / total_pixels as f32) * 100.0
+        );
+    }
+
+    println!();
 }
 
 fn write_image(path: &str, pixels: &[u8]) -> anyhow::Result<()> {
@@ -149,5 +170,5 @@ fn ray_color(world: &[Object], ray: &Ray, background: Rgb, rng: &mut impl Rng, d
         }
     } else {
         background
-}
+    }
 }
